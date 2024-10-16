98853f4ce11226ad72751b8f5241e06f4e14b57a
Sean <62218565+Kingcake225@users.noreply.github.com>
2024-10-16 19:59:00 +0100

    Updated With MODELS

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Extended User Model
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # Creates one-to-one relationship between this model and the default user model, on deleting of user delete the profile, but don't delete the user if profile deleted.
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
